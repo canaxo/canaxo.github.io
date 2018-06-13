d0c423959210d7f93ad7fc2268504f4cddbc8f76
nowlow <nowlow77@gmail.com>
2018-06-13 15:30:05 +0200

    Night mode added

--- js/app.js
+++ js/app.js
@@ -24,16 +24,51 @@ window.onload = () => {
 	} else {
 		createCookie('timer', '0', 99999)
 	}
+	
+	if(readCookie('theme') != null) {
+		if(readCookie('theme') == 'night') {
+			day.style.display = 'inline'
+			night.style.display = 'none'
+			document.body.classList.add('night')
+			for (var i = 0; i < els.length; i++) {
+				els[i].classList.add('night')
+			}
+		} else if(readCookie('theme') == 'day') {
+			night.style.display = 'inline'
+			day.style.display = 'none'
+			document.body.classList.add('day')
+			for (var i = 0; i < els.length; i++) {
+				els[i].classList.add('day')
+			}
+		}
+	} else {
+		day.style.display = 'inline'
+		night.style.display = 'none'
+		createCookie('theme', 'day', 99999)
+		document.body.classList.add('day')
+		for (var i = 0; i < els.length; i++) {
+			els[i].classList.add('day')
+		}
+	}
+
+	menu.style.height = screen.height + 'px'
 }
 
 var subscribe = document.getElementById('subscribe')
 var video = document.getElementById('video')
 var menu = document.getElementById('menu')
 var menudisplayer = document.getElementById('menudisplayer')
+var day = document.getElementById('day')
+var night = document.getElementById('night')
+var els = document.body.querySelectorAll("*")
 
 setInterval(() => {
 	createCookie('timer', video.currentTime, 99999)
 }, 500)
+
+window.onresize = () => {
+	menu.style.height = screen.height + 'px'
+}
 
 function createCookie(name,value,days) {
 	if (days) {
@@ -82,29 +117,48 @@ menudisplayer.onclick = () => {
 	setTimeout(() => {
 		if(menu.style.visibility == 'hidden') {
 			menu.style.visibility = 'visible'
-			menu.style.height = (screen.height - document.querySelector('.header').offsetHeight - document.querySelector('.footer').offsetHeight) + 'px'
+			menu.style.width = '350px'
 			console.log(menu.style.visibility)
 		} else {
 			menu.style.visibility = 'hidden'
-			menu.style.height = '0'
+			menu.style.width = '0'
 		}
 	}, 50)
 }
+
+day.onclick = () => {
+	createCookie('theme', 'day', 99999)
+	night.style.display = 'inline'
+	day.style.display = 'none'
+	document.body.classList.add('day')
+	document.body.classList.remove('night')
+	for (var i = 0; i < els.length; i++) {
+		els[i].classList.add('day')
+		els[i].classList.remove('night')
+	}
+}
+
+night.onclick = () => {
+	createCookie('theme', 'night', 99999)
+	day.style.display = 'inline'
+	night.style.display = 'none'
+	document.body.classList.add('night')
+	document.body.classList.remove('day')
+	for (var i = 0; i < els.length; i++) {
+		els[i].classList.add('night')
+		els[i].classList.remove('day')
+	}
+}
+
 document.onclick = (e) => {
 	if(menu.style.visibility != 'hidden') {
 		var menuposXs = menu.offsetLeft
 		var menuposXe = menuposXs + menu.offsetWidth
 		var menuposYs = menu.offsetTop
 		var menuposYe = menuposYs + menu.offsetHeight
-		console.log('Xs = ' + menuposXs)
-		console.log('Xe = ' + menuposXe)
-		console.log('Ys = ' + menuposYs)
-		console.log('Ye = ' + menuposYe)
-		console.log('Client X = ' + e.clientX)
-		console.log('Client Y = ' + e.clientY)
 		if(!(menuposXs <= e.clientX && e.clientX <= menuposXe) || !(menuposYs <= e.clientY && e.clientY <= menuposYe)) {
 			menu.style.visibility = 'hidden'
-			menu.style.height = '0'
+			menu.style.width = '0'
 		}
 	}
 }
