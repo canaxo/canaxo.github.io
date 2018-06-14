e2b05ebd25fa8af86e87dc3ea1336cd71a7b5590
nowlow <nowlow77@gmail.com>
2018-06-14 11:44:01 +0200

    Minor update

--- js/app.js
+++ js/app.js
@@ -52,6 +52,7 @@ window.onload = () => {
 	}
 
 	menu.style.height = document.clientHeight + 'px'
+	console.log(navigator.appVersion)
 }
 
 var container = document.querySelector('.container')
@@ -70,31 +71,6 @@ setInterval(() => {
 		console.log(sessionStorage.getItem('time'))
 	}
 }, 500)
-
-function createCookie(name,value,days) {
-	if (days) {
-		var date = new Date();
-		date.setTime(date.getTime()+(days*24*60*60*1000));
-		var expires = "; expires="+date.toGMTString();
-	}
-	else var expires = "";
-	document.cookie = name+"="+value+expires+"; path=/";
-}
-
-function readCookie(name) {
-	var nameEQ = name + "=";
-	var ca = document.cookie.split(';');
-	for(var i=0;i < ca.length;i++) {
-		var c = ca[i];
-		while (c.charAt(0)==' ') c = c.substring(1,c.length);
-		if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
-	}
-	return null;
-}
-
-function eraseCookie(name) {
-	createCookie(name,"",-1);
-}
 
 subscribe.onclick = () => {
 	if(readCookie('subscribed') == 'false') {
