d94cf507a03b2a29c54a6a3d654cc6884551c062
nowlow <nowlow77@gmail.com>
2018-06-14 16:44:09 +0200

    minor update

--- js/app.js
+++ js/app.js
@@ -45,10 +45,10 @@ window.onload = () => {
 		}
 	}
 	
-	if(sessionStorage.getItem('time') != null) {
-		video.currentTime = sessionStorage.getItem('time')
+	if(sessionStorage.getItem('time' + document.getElementById('episodename').innerHTML != null) {
+		video.currentTime = sessionStorage.getItem('time' + document.getElementById('episodename').innerHTML)
 	} else {
-		sessionStorage.setItem('time', 0)
+		sessionStorage.setItem('time' + document.getElementById('episodename').innerHTML, 0)
 	}
 	
 	menu.style.height = document.clientHeight + 'px'
@@ -72,7 +72,7 @@ if(ua.indexOf('mobile') > -1) isMobile = true
 
 setInterval(() => {
 	if(!video.paused) {
-		sessionStorage.setItem('time', video.currentTime)
+		sessionStorage.setItem('time' + document.getElementById('episodename').innerHTML, video.currentTime)
 	}
 }, 500)
 
