1f3ae44510c4e6c13aac091159a18b97c179408b
nowlow <nowlow77@gmail.com>
2018-06-16 12:07:30 +0200

    WOW I AM TRYING SOMETHING

---
+++ js/pagebuilder.js
@@ -0,0 +1,18 @@
+var meta = document.head.querySelector('meta[name="video"]')
+
+let video_request = fetch('JSON_content/videos/' + meta.getAttribute('content') + '.json', {
+	method: 'GET',
+}).then(function (response) {
+	return response.json()
+}).then(function (json) {
+	var video = document.getElementById('video')
+	var episodename = document.getElementById('episodename')
+	var seriesname = document.getElementById('seriesname')
+	var episode = document.getElementById('episode')
+	var description = document.getElementById('description')
+	video.src = 'http://93.4.85.60/NowlowPlayer/src/videos/' + meta.getAttribute('content').toLowerCase + '.mp4'
+	episodename.innerHTML = json.episodename
+	seriesname.innerHTML = json.seriesname
+	episode.innerHTML = json.episode
+	description.innerHTML = json.description
+})
