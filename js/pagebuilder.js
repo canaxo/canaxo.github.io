c7b41a3cf15394c7b2d20c266f1c6cdab7eb055d
nowlow <nowlow77@gmail.com>
2018-06-16 17:51:08 +0200

    changed something...

--- js/pagebuilder.js
+++ js/pagebuilder.js
@@ -10,26 +10,49 @@ function extractUrlParams(){
 
 var urlPar = extractUrlParams()
 
-let video_request = fetch('JSON_content/videos/' + urlPar['id'] + '.json', {
+var video = document.getElementById('video')
+var episodename = document.getElementById('episodename')
+var seriesname = document.getElementById('seriesname')
+var episode = document.getElementById('episode')
+var description = document.getElementById('description')
+var next = document.querySelector('.nextVideo')
+
+let video_request = fetch('JSON_content/videos.json', {
 	method: 'GET',
 }).then(function (response) {
 	return response.json()
 }).then(function (json) {
-	var video = document.getElementById('video')
-	var episodename = document.getElementById('episodename')
-	var seriesname = document.getElementById('seriesname')
-	var episode = document.getElementById('episode')
-	var description = document.getElementById('description')
-	var next = document.querySelector('.nextVideo')
-	console.log(json)
-	if(video != null) {
-		video.src = 'http://93.4.85.60/NowlowPlayer/src/videos/' + urlPar['id'].toLowerCase() + '.mp4'
-		episodename.innerHTML = json.episodename
-		seriesname.innerHTML = json.seriesname
-		episode.innerHTML = json.episode
-		description.innerHTML = json.description
-		next.href = urlPar['id'] == 'GravityFallsS02E01' ? 'watch.html?id=RickAndMortyS01E07' : 'watch.html?id=GravityFallsS02E01'
-		document.title = json.seriesname + ' - ' + json.episode + ' - Nowlow\'s video player'
+	var exists = false
+	for(var i=0; i < json.length; i++) {
+		if(urlPar['id'] == json[i]) exists = true
 	}
-	
+	if(exists) {
+		fetch('JSON_content/videos/' + urlPar['id'] + '.json', {
+			method: 'GET',
+		}).then(function (response) {
+			return response.json()
+		}).then(function (video_json) {
+			if(video != null) {
+				video.src = 'http://93.4.85.60/NowlowPlayer/src/videos/' + urlPar['id'].toLowerCase() + '.mp4'
+				episodename.innerHTML = video_json.episodename
+				seriesname.innerHTML = video_json.seriesname
+				episode.innerHTML = video_json.episode
+				description.innerHTML = video_json.description
+				var rand = Math.floor((Math.random() * json.length) + 1)
+				while(json[rand] == urlPar[id]) {
+					rand = Math.floor((Math.random() * json.length) + 1)
+				}
+				next.href = 'watch.html?id=' + json[rand]
+				document.title = json.seriesname + ' - ' + video_json.episode + ' - Nowlow\'s video player'
+			}
+		})
+	} else {
+		episodename.innerHTML = 'Video not found'
+		seriesname.innerHTML = 'Wrong URL'
+		episode.innerHTML = 'S00E00'
+		description.innerHTML = 'Please make us a feedback '
+		next.href = urlPar['id'] == 'index.html'
+		next.innerHTML = 'Go home'
+		document.title ='Video not found - Nowlow\'s video player'
+	}
 })
